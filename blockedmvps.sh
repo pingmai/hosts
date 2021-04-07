659e039f8e6856a3cffd625ea115ddf3aa2df170
ping <ping@stepnet.com>
2021-04-06 19:08:27 -0700

    check if hosts exists in zones.blocked.*

--- blockedmvps.sh
+++ blockedmvps.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 set -o errexit -o pipefail
 
+zfiles=`find /var/named/usr/local/etc/namedb -maxdepth 1 \
+	-name "zones.blocked*" -a ! -name zones.blocked.mvps`
+
 curl -s https://winhelp2002.mvps.org/hosts.txt |\
-	sed -n 's:::; s:^0.0.0.0 \([^ ]*\).*:zone \"\1\" { type master; file \"master/null.zone.db\"; };:p'
+	sed -n 's:::; s:^0.0.0.0 \([^ ]*\).*:\1:p' |\
+	while read h ; do
+		if ! grep -q $h $zfiles; then
+			echo "zone \"$h\" { type master; file \"master/null.zone.db\"; };"
+		fi
+	done
 
