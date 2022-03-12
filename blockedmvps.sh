21e447dae56e3b3948428b85198598f618cb5de0
ping <ping@stepnet.com>
2022-03-12 10:19:38 -0800

    full paths

--- blockedmvps.sh
+++ blockedmvps.sh
@@ -1,10 +1,10 @@
-#!/usr/bin/env bash
+#!/usr/local/bin/bash
 set -o errexit -o pipefail
 
 zfiles=`find /var/named/usr/local/etc/namedb -maxdepth 1 \
 	-name "zones.blocked*" -a ! -name zones.blocked.mvps`
 
-curl -s https://winhelp2002.mvps.org/hosts.txt |\
+/usr/local/bin/curl -s https://winhelp2002.mvps.org/hosts.txt |\
 	sed -n 's:::; s:^0.0.0.0 \([^ ]*\).*:\1:p' |\
 	while read h ; do
 		if ! grep -q $h $zfiles; then
